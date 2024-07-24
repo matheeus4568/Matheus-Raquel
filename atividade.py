53e70729dc02855f0acda39e1bbcc8ff55995d5e
Matheeus4568 <rodriguesmatheus4568@gmail.com>
2024-07-24 09:25:18 -0300

    final

--- atividade.py
+++ atividade.py
@@ -1,35 +1,32 @@
 from atividadee import Raquel
 
+#RODAR ESTE ARQUIVO
+
 class Matheus(Raquel):
     __altura_cm = 0
     gosta_de = ''
-    
+
     def setAtr(self):
         self.nome = input("Digite um nome: ")
         self._idade = input("Digite uma idade: ")
         self.__cpf = input("Digite um cpf: ")
-        try:
-            self.__altura = input("Digite uma altura em centímetros: ")
-        except:
-            print("Valor inválido. Digite um número inteiro.")
-            self.__altura = input("Digite uma altura em centímetros: ")
+        self.__altura_cm = int(input("Digite uma altura em centímetros: "))
         self.gosta_de = input("Digite uma coisa que tu gosta: ")
-    
+
     def getDados(self):
-        return '''=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-nome = {self.nome}
-idade = {_self._idade}
-cpf = {self.__cpf}
-altura em cm: {__self.alturacm}
-{self.nome} gosta de {self.gosta_de}
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-='''
+        print('Nome:' + self.nome)
+        print('idade:' + self._idade)
+        print('cpf:' + self.__cpf)
+        print("altura em cm:", self.__altura_cm)
+        print(self.nome + "gosta de:" + self.gosta_de)
 
     def gritar(self):
-        return "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-    
+        return '''=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'''
+
 
 teste = Matheus()
 teste.setAtr()
-print(teste.getDados())
+teste.getDados()
 print(teste.gritar())
-
+print(teste.falar())
